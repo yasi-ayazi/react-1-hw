9fe5f8c33e14e36e681d529f4ca2ab7317842dc5
yasi <yasi.ayazi@gmail.com>
2025-06-10 12:11:13 +0200

    Add NavItem component for improved navigation structure

---
+++ components/ui/NavItem.js
@@ -0,0 +1,18 @@
+'use client';
+
+import styles from "./Navbar.module.css";
+
+const NavItem = ({ title, link, isActive, index }) => {
+  return (
+    <li key={index}>
+      <a
+        href={link}
+            className={isActive ? styles.navItemActive : styles.navItem}
+      >
+        {title}
+      </a>
+    </li>
+  );
+};
+
+export { NavItem };
