b45cc48629431253348c2d8ebc30b34d743b9746
yasi <yasi.ayazi@gmail.com>
2025-06-10 12:11:28 +0200

    Add SocialMediaItem component for enhanced social media integration in Footer

---
+++ components/ui/SocialMediaItem.js
@@ -0,0 +1,18 @@
+'use client';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './Footer.module.css';
+
+export const SocialMediaItem = ({ url, title, icon }) => {
+  return (
+    <li className={styles.socialItem}>
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.socialLink}
+      >
+        <FontAwesomeIcon icon={icon} /> <span>{title}</span>
+      </a>
+    </li>
+  );
+};
